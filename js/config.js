84ae818b51ed9b72cb0f27b601aa24669bdb1f88
carehospitalsportal <muralimanohar.co@gmail.com>
2026-09-23 13:24:57 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -6,6 +6,9 @@
   Do NOT use the service_role/secret key here.
 */
 window.SAFE_ROUTE_CONFIG = {
-  SUPABASE_URL: "YOUR_SUPABASE_URL",
-  SUPABASE_ANON_KEY: "YOUR_SUPABASE_ANON_OR_PUBLISHABLE_KEY"
+  SUPABASE_URL: "https://rtjbfampqsjkzpthiysz.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_aD0s-B29RpzriIKqx-INEw_gi8Q6_Ln"
 };
+
+
+
